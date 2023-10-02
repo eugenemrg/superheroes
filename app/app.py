16a4f8b0e89c3e374242f35b32fba7fe79350131
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-02 12:13:38 +0300

    Added CORS handling using flask-cors package, fixes CORS errors on client/browser

--- app/app.py
+++ app/app.py
@@ -5,11 +5,13 @@ from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
+from flask_cors import CORS
 import json
 
 from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
